708fc37ddbe35cce17b48f1b42e0a27fb446807d
Tesloxi <63243023+Tesloxi@users.noreply.github.com>
2020-07-20 14:28:38 +0200

    Add files via upload

--- classes.py
+++ classes.py
@@ -11,11 +11,11 @@ class Application(Frame):
         
     def menu(self):
         self.master.title('Gomoku')
-        Label(self, text='GOMOKU').pack(side='top', padx=100, pady=40)
+        Label(self, text='GOMOKU').pack(side='top', pady=20)
         self.frame = LabelFrame(self, text='Rules')
-        Label(self.frame, text="Le joueur qui a choisi ou obtenu par un tirage au sort les pions noirs (et que l'on appelle Noir par convention) joue toujours le premier en plaçant son premier pion sur l'intersection centrale du damier. Blanc doit alors poser son pion sur l'une des 8 intersections adjacentes au pion noir. Noir fait la même chose, et ainsi de suite, le but du jeu étant de prendre l'adversaire de vitesse et de réussir le premier à aligner 5 pions de sa couleur, dans les trois directions possibles : vertical, horizontal ou diagonal.").pack()
-        Button(self.frame, text='PLAY', command=self.play).pack()
-        self.frame.pack(side='top')
+        Label(self.frame, text="The player who has drawn black pieces always plays first\nby placing his first piece on the central intersection of the checkerboard.\nWhite must then place his pawn on one of the 8 intersections adjacent to the Black pawn.\nBlack does the same, and so on, the object of the game being to take the opponent by speed and\nto be the first to line up 5 checkers of his color, in the three possible directions: vertical, horizontal or diagonal.").pack()
+        Button(self.frame, text='PLAY', command=self.play).pack(pady=40, ipadx=20, ipady=10)
+        self.frame.pack(side='top', padx=20, pady=10)
 
     def play(self):
         self.board = Board(self)
@@ -26,30 +26,34 @@ class Board(Application):
         self.player = 1
         self.finished = False
         self.first_token = True
+        self.nb_token_placed = 0
         self.board = Canvas(app, width=board_size, height=board_size, background="#fb9d4e")
         for i in range(square_size // 2, board_size - square_size // 2 + 1, square_size):
             self.board.create_line(square_size / 2, i, board_size - square_size / 2, i)
             self.board.create_line(i, square_size / 2, i, board_size - square_size / 2)
         self.board.create_oval(nb_squares // 2 * 40 + 2, nb_squares // 2 * 40 + 2, nb_squares // 2 * 40 + 38, nb_squares // 2 * 40 + 38, outline='black', fill='black')
         board_arr[nb_squares // 2][nb_squares // 2] = 'w'
-        self.board.pack()
+        self.board.pack(expand=True)
         self.board.bind('<Button-1>', self.click)
 
     def click(self, event):
         """Draw a token on the nearest cross of the click"""
         x, y = round(float(event.x - 20) / 40) * 40 + 20, round(float(event.y - 20) / 40) * 40 + 20
         if board_arr[y // 40][x // 40] == 'e' and (board_arr[y // 40 - 1][x // 40] != 'e' or board_arr[y // 40 - 1][x // 40 - 1] != 'e' or board_arr[y // 40][x // 40 - 1] != 'e' or board_arr[y // 40 + 1][x // 40 - 1] != 'e' or board_arr[y // 40 + 1][x // 40] != 'e' or board_arr[y // 40 + 1][x // 40 + 1] != 'e' or board_arr[y // 40][x // 40 + 1] != 'e' or board_arr[y // 40 - 1][x // 40 + 1] != 'e'):
-            board_arr[y // 40][x // 40] = 'w'
+            self.nb_token_placed += 1
             if self.player == 0:
+                board_arr[y // 40][x // 40] = 'd'
                 self.board.create_oval(x - 18, y - 18, x + 18, y + 18, outline='black', fill='black')
                 self.player = 1
             else:
+                board_arr[y // 40][x // 40] = 'w'
                 self.board.create_oval(x - 18, y - 18, x + 18, y + 18, outline='white', fill='white')
                 self.player = 0
             self.board.pack()
             self.isFinished(x, y)
-            if self.finished:
+            if self.finished or self.nb_token_placed == nb_tokens_max:
                 self.board.bind('<Button-1>', self.r)
+                print(self.nb_token_placed)
                 print('finished')
         return
 
@@ -64,6 +68,7 @@ class Board(Application):
         or (j < nb_squares - 4 and board_arr[i][j + 1] == a and board_arr[i][j + 2] == a and board_arr[i][j + 3] == a and board_arr[i][j + 4] == a): # top left bottom right
             self.finished = True
             print(a)
+            print(i, j)
             return
 
     def r(self, *args):
--- constants.py
+++ constants.py
@@ -3,6 +3,7 @@
 square_size = 40
 nb_squares = 19
 board_size = square_size * nb_squares
+nb_tokens_max = 120
 board_arr = []
 for _ in range(nb_squares):
     board_arr.append(['e'] * nb_squares)
